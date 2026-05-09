8c44a9942c34b254716896e926dcb94231bf791f
Soumil Konar <soumil.konar@boeing.com>
2026-05-09 16:27:07 +0530

    Fix KeyboardOptions import

--- app/src/main/java/com/soumil/moneytracker/ui/components/Dialogs.kt
+++ app/src/main/java/com/soumil/moneytracker/ui/components/Dialogs.kt
@@ -41,7 +41,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
